d901ddd5517d459d3ef2cd19a582cdeb980d071c
Shirley <shirley.apellido@gmail.com>
2020-07-29 20:41:17 +0800

    adding header component

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const SearchBox = ({searchfield, searchChange}) => {
+    console.log('searchBox');
     return(
         <div className='pa2 '>
             <input className='pa3 ba b--green bg-lightest-blue' 
